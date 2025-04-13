94aeaa1d4afc795df95d33c4af10d22047e9903f
ShumayS1 <shumaenko.18@mail.ru>
2025-04-13 14:36:47 +0300

    1

--- Models/CompatibilityAnalysis.cs
+++
@@ -1,8 +0,0 @@
-namespace DIPLOMKA.Models;
-
-public class CompatibilityAnalysis
-{
-    public string Name { get; set; }
-    public string Description { get; set; }
-    // Логика анализа совместимости
-}
--- Models/RecommendationService.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;  // Для List<>
-using System.Threading.Tasks;     // Для Task<>
-using DIPLOMKA.Models;     
-namespace DIPLOMKA.Models;
-
-public class RecommendationService
-{
-    // Логика для рекомендаций
-}
